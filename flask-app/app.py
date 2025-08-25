caff144239a0418e10b660185be3dd0288d24673
khitish1234454234234 <khitishdas2003@gmail.com>
2025-08-25 17:17:23 +0530

    initial commit

---
+++ flask-app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app,origins="*")
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__=="__main__":
+    app.run(debug=True, port=3000)
